321d192caf939d5176751f762895845cd8c39ae1
Aiza Savvinova <hmstrayza@yandex.ru>
2023-09-26 23:36:50 +0300

    change url

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -45,7 +45,7 @@ class ApiAuth {
   }
 }
 const apiAuth = new ApiAuth({
-  baseUrl: "http://localhost:3000",
+  baseUrl: "http://api.mesto.pr15.nomoredomainsrocks.ru",
   headers: {
     "Content-Type": "application/json",
   },
